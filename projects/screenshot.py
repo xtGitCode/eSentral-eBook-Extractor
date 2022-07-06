80410d22671e48c860e5792c21866cfc466784e1
Xiaothung <44636208+Xiaothung@users.noreply.github.com>
2022-07-06 22:54:49 +0800

    changes

--- projects/screenshot.py
+++ projects/screenshot.py
@@ -1,13 +1,18 @@
 import pyautogui
 import pynput
+import os
 import os.path 
 from pynput.mouse import Listener
 
+filepath = os.getenv('APPDATA') + "\Screenshot"
+if not os.path.exists(filepath):
+    os.makedirs(filepath)
+    
 def on_click(x, y, button, pressed):
     i=0
-    while os.path.exists(r'C:\Users\xiaot\Downloads\Screenshots\image%s.jpg'%i):
+    while os.path.exists(filepath + r'\image%s.jpg'%i):
         i += 1
-    pyautogui.screenshot(r'C:\Users\xiaot\Downloads\Screenshots\image%s.jpg'%i)
+    pyautogui.screenshot(filepath + r'\image%s.jpg'%i)
 
 
 with Listener(on_click=on_click) as listener:
